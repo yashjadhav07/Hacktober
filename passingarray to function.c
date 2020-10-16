d9e7f30f458846529f93456f3c431c0408abd3c2
Ruturaj Deshmukh <51050421+RuturajDeshmukh@users.noreply.github.com>
2020-10-17 01:01:18 +0530

    Add files via upload

---
+++ passingarray to function.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+int size,i;
+printf("enter the size of array");
+scanf("%d",&size);
+int disp[size];
+for(i=0;i<size;i++)
+scanf("%d",&disp[i]);
+display(disp,size);
+
+}
+int display(int a[],int s)
+{
+int i=0;
+for(i=0;i<s;i++)
+{
+printf("%d\n",a[i]);
+}
+
+
+}
